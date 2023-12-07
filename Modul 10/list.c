8c0904c71149791a8392b90c1cde8cefaaceb754
Hyuga Dewanto Kojyro <89519700+hyugenk@users.noreply.github.com>
2023-12-07 09:21:44 +0700

    Create list.c
    Create new file

---
+++ Modul 10/list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <dirent.h>
+main(){
+	struct dirent
+	**namelist; int n,i;
+	char pathname[100];
+	getcwd(pathname);
+	
+	n = scandir(pathname, &namelist, 0,alphasort);
+	if(n < 0)
+		printf("Error\n");
+	else
+		for(i=0; i<n; i++) if(namelist[i]->d_name[0] !='.')
+			printf("%-20s", namelist[i]->d_name);
+}
